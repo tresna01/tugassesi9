4c6edbdf738ca390f7450162d92a887414478708
tresna01 <148548388+tresna01@users.noreply.github.com>
2024-05-13 06:33:18 +0700

    Add files via upload

---
+++ var1.py
@@ -0,0 +1,13 @@
+number = []
+number.insert(0,6)
+number.insert(1,5)
+number.insert(2,10)
+print(number)
+number.remove(6)
+number.append(1)
+number.append(9)
+number.sort()
+print(number)
+number.pop(-1)
+number.reverse()
+print(number)
